035e0b409d97d5a80fc8be350dbfb695ff3822b5
andela-amagana <alex.magana@andela.com>
2016-11-29 19:48:11 +0300

    chore(modules): add response messages shared module

---
+++ app/controllers/concerns/messages.rb
@@ -0,0 +1,39 @@
+module Concerns
+  module Messages
+    def record_not_found
+      "Record not found."
+    end
+
+    def invalid_request
+      "Invalid request."
+    end
+
+    def login_successful
+      "Login successful."
+    end
+
+    def already_logged_in
+      "You're already logged in."
+    end
+
+    def invalid_login_credentials
+      "Invalid username/password."
+    end
+
+    def logout_successful
+      "Logout successful."
+    end
+
+    def user_deletion_successful
+      "User deleted successfully."
+    end
+
+    def bucket_list_deletion_successful
+      "Bucket list deleted successfully."
+    end
+
+    def bucket_list_item_deletion_successful
+      "Bucket list item deleted successfully."
+    end
+  end
+end
