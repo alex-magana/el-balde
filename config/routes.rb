2b57bf18902e041060b59b86053c21fdd2677483
andela-amagana <alex.magana@andela.com>
2016-11-29 18:05:12 +0300

    feat(version_api): add verioned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show, :create, :update, :destroy]
+      resources :bucket_lists, only: [:index, :show, :create, :update, :destroy], path: :bucketlists do
+        resources :bucket_list_items, only: [:index, :show, :create, :update, :destroy], path: :items
+      end
+
+      post "auth/login", to: "authentications#login"
+      get "auth/logout", to: "authentications#logout"
+    end
+  end
 end
