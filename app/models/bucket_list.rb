0c857ba67e3d9965cf2acac53235dc6737bca1ae
andela-amagana <alex.magana@andela.com>
2016-11-29 16:00:37 +0300

    feat(create_bucketlist): add model bucket_list

---
+++ app/models/bucket_list.rb
@@ -0,0 +1,23 @@
+class BucketList < ApplicationRecord
+  extend Concerns::Paginate
+
+  belongs_to :user
+  has_many :bucket_list_items, dependent: :destroy
+
+  VALID_REGEX_NAME = /[a-zA-Z]+/
+
+  validates :name,
+            presence: true,
+            format: { with: VALID_REGEX_NAME },
+            length: { minimum: 3,
+                      message: "Too short. The minimum length"\
+                               " is 3 characters." },
+            uniqueness: {
+              scope: :user_id,
+              message: "A list with a similar name already exists."
+            }
+
+  def self.search_by_name(name)
+    where("name LIKE ?", "%#{name}%")
+  end
+end
